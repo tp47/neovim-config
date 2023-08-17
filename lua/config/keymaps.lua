c1dca009471bc901f972c8d59198d363994e4742
Ratmir Tsaregorodtsev <RTsaregorodtsev@alabuga.ru>
2023-08-17 09:19:02 +0300

    Fix mapleader

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.g.mapleader = ","
-
 vim.keymap.set('n', '<leader>w', ':HopWord<cr>', { noremap = true, silent = true, desc = 'Goto word' })
