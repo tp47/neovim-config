b2c6c949a513e42b20fc1e4fbdafe7cb5fef222f
Ratmir Tsaregorodtsev <RTsaregorodtsev@alabuga.ru>
2023-08-17 09:13:32 +0300

    Init

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.g.mapleader = ","
+
+vim.keymap.set('n', '<leader>w', ':HopWord<cr>', { noremap = true, silent = true, desc = 'Goto word' })
